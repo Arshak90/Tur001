9e98158ebc4e14e11e3c0765eef180e1fe448bf1
Arshak Askaryan <arshak90@list.ru>
2017-01-30 00:01:16 +0300

    DB: Add tables

--- src/main/java/portfolio/PortfolioDao.java
+++ src/main/java/portfolio/PortfolioDao.java
@@ -1,7 +1,35 @@
 package portfolio;
 
+import Core.Interface.Dao;
+
+import java.util.List;
+
 /**
  * Created by arshak.askaryan on 1/25/2017.
  */
-public class PortfolioDao {
+public class PortfolioDao implements Dao<Portfolio>{
+    @Override
+    public List<Portfolio> getAll() {
+        return null;
+    }
+
+    @Override
+    public Portfolio getById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void insert(Portfolio item) {
+
+    }
+
+    @Override
+    public boolean update(Portfolio item) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        return false;
+    }
 }
